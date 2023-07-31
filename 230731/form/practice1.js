cc43ab69f3384cbbeb52fbfe339e3523187801f8
best0611 <best970611@naver.com>
2023-07-31 15:15:52 +0900

    feat: 230731 실습2

--- 230731/form/practice1.js
+++ 230731/form/practice1.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
