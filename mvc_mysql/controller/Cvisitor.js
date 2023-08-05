9e7c2f3e59c15bc0d80748cc940cbad6579e8ca9
best0611 <best970611@naver.com>
2023-08-05 19:44:28 +0900

    router practice

--- mvc_mysql/controller/Cvisitor.js
+++ mvc_mysql/controller/Cvisitor.js
@@ -4,5 +4,32 @@ exports.main = (req, res) => {
   res.render("index");
 };
 exports.getVisitors = (req, res) => {
-  res.render("visitor", { data: visitor.getVisitors() });
+  // [before]
+  //   res.render("visitor", { data: visitor.getVisitors() });
+  // mysql 연결 후
+  visitor.getVisitors((result) => {
+    // console.log("Cvisitor", result);
+    // [{}, {}, {}] 형식인 것을 확인할 수 있음
+    res.render("visitor", { data: result });
+  });
+};
+exports.write = (req, res) => {
+  // console.log("write", req.body);
+  visitor.write(req.body, (result) => {
+    res.send(result);
+  });
+};
+
+exports.delete = (req, res) => {
+  console.log(req.body.id);
+  // res.send(req.body.id);
+  visitor.delete(req.body.id, (result) => {
+    res.send(result);
+    console.log(result);
+  });
+};
+
+exports.patch = (req, res) => {
+  console.log(req.body);
+  visitor.patch();
 };
--- mvc_mysql/model/Visitor.js
+++ mvc_mysql/model/Visitor.js
@@ -1,6 +1,52 @@
-exports.getVisitors = () => {
-  return [
-    { id: 1, name: "홍길동", comment: "내가 왔다." },
-    { id: 2, name: "이찬혁", comment: "으라차차" },
-  ];
+// mysql 연결 전
+// exports.getVisitors = () => {
+//   return [
+//     { id: 1, name: "홍길동", comment: "내가 왔다." },
+//     { id: 2, name: "이찬혁", comment: "으라차차" },
+//   ];
+// };
+
+const mysql = require("mysql");
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "1234",
+  database: "kdt8",
+});
+
+// mysql 연결 후
+exports.getVisitors = (callback) => {
+  const sql = "SELECT * FROM visitor;";
+  conn.query(sql, (err, rows) => {
+    if (err) throw err;
+    // console.log("Visitor : ", rows);
+    callback(rows);
+  });
 };
+
+exports.write = (info, callback) => {
+  // console.log(info.username, info.content);
+  const sql1 = `INSERT INTO visitor (name, comment) VALUES ('${info.username}','${info.content}');`;
+  const sql2 = "SELECT * FROM visitor";
+
+  conn.query(sql1, (err, rows) => {
+    if (err) throw err;
+    conn.query(sql2, (err, rows) => {
+      if (err) throw err;
+      console.log(rows);
+      callback(rows);
+    });
+  });
+};
+
+exports.delete = (id, callback) => {
+  console.log(id);
+  const sql = `DELETE FROM visitor WHERE id = ${id}`;
+  conn.query(sql, (err, rows) => {
+    if (err) throw err;
+    console.log(rows);
+    callback(id);
+  });
+};
+
+exports.patch = () => {};
