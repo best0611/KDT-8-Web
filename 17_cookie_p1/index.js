96abc147a816dc9fe792bcb104e1634ca3fe601b
best0611 <best970611@naver.com>
2023-08-14 12:04:16 +0900

    230814 session p1

--- 17_cookie_p1/index.js
+++ 17_cookie_p1/index.js
@@ -6,26 +6,25 @@ const PORT = 8000;
 app.set("view engine", "ejs");
 app.use(cookieParser());
 const cookieConfig = {
+  httpOnly: true,
   maxAge: 60 * 1000 * 60 * 24,
 };
 
 app.get("/", (req, res) => {
   res.render("index");
+  // res.render('index', {popup: req.cookies.modal})
 });
 
 app.get("/stopShow", (req, res) => {
   res.cookie("stopShow", "checked", cookieConfig);
   res.send(req.cookies.stopShow);
-  // console.log(req.cookies.stopShow);
+  // res.send({result: true, msg: '쿠키 생성 완료'})
 });
 
 app.get("/clearCookie", (req, res) => {
   res.clearCookie("stopShow", "checked", cookieConfig);
   res.send("clear cookie");
 });
-// app.get("/getCookie", (req, res) => {
-//   res.send(req.cookies);
-// });
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
--- 17_session_p1/index.js
+++ 17_session_p1/index.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = 8000;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(
   session({
@@ -13,11 +15,47 @@ app.use(
   })
 );
 
+const userInfo = { id: "kdt8", pw: "1234" };
+
 //router
 app.get("/", (req, res) => {
-  res.render("index");
+  // session 유무를 확인
+  const user = req.session.user;
+  if (user === undefined) {
+    res.render("index", { isLogin: false });
+  } else {
+    res.render("index", { isLogin: true, user: user });
+  }
+});
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
+app.post("/login", (req, res) => {
+  if (req.body.id === userInfo.id && req.body.pw === userInfo.pw) {
+    // 세션에 사용자 정보를 저장
+    req.session.user = req.body.id;
+    res.redirect("/");
+  } else {
+    res.send(
+      `<script>alert('로그인실패'); document.location.href='/';</script>`
+    );
+  }
+});
+
+app.get("/logout", (req, res) => {
+  const user = req.session.user;
+  if (user === undefined) {
+    res.send(
+      `<script>alert('잘못된 접근입니다.'); document.location.href='/';</script>`
+    );
+  } else {
+    req.session.destroy(() => {
+      res.clearCookie("connect.sid");
+      res.redirect("/");
+    });
+  }
+});
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
